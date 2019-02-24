cd5560b2d9f51a159ebb607bfa82994c1b43629f
John Alejandro Gonzalez <johnalejandrog.g4@gmail.com>
2019-02-23 20:18:31 -0500

    added connection to mongo cluster database

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/ATM_db', {
+mongoose.connect('mongodb://test0:dVxdMyx1HmhxM1sH@cluster0-shard-00-00-ojqdw.mongodb.net:27017,cluster0-shard-00-01-ojqdw.mongodb.net:27017,cluster0-shard-00-02-ojqdw.mongodb.net:27017/ATM_db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
