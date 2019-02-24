5a236b8283b1e74d4d7ace3ac12c12531c837efa
John Alejandro Gonzalez <johnalejandrog.g4@gmail.com>
2019-02-24 14:32:52 -0500

    añadida funcionalidad para cambiar tu eleccion al momento de requerir un prestamo

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -6,31 +6,60 @@ $(document).ready(function () {
             e.preventDefault();
         };
     });
-    $('.amount').click(function(e){
-        let container = $('#amount-container')
-        let form = $('#form-group')
-        container.empty();
-        container.append(this)
-        form.append(`<input type="hidden" name="amount" value="${this.value}">`)
-        console.log(this.value, container)
-        
-    })
-    $('.dues').click(function(e){
-        let container = $('#dues-container')
-        let form = $('#form-group')
-        container.empty();
-        container.append(this)
-        form.append(`<input type="hidden" name="dues" value="${this.value}">`)
-        console.log(this.value, container)
-    })
+    listenCreditOptions()
+    //Sistema de prestamos
+    function listenCreditOptions() {
+        $('.amount').click(function (e) {
+            let container = $('#amount-container')
+            let form = $('#form-group')
+            container.empty();
+            container.append(this)
+            container.append(`<button type="button" class="btn btn-warning btn-sm ml-2" id="backA"><i class="fas fa-arrow-left"></i></button>`)
+            listenBackOption()
+            $('#amountValue').remove()
+            form.append(`<input type="hidden" name="amount" value="${this.value}" id="amountValue">`)
+            // console.log(this.value, container)
+        })
+        $('.dues').click(function (e) {
+            let container = $('#dues-container')
+            let form = $('#form-group')
+            container.empty();
+            container.append(this)
+            container.append(`<button type="button" class="btn btn-warning btn-sm ml-2" id="backD"><i class="fas fa-arrow-left"></i></button>`)
+            listenBackOption()
+            $('#duesValue').remove()
+            form.append(`<input type="hidden" name="dues" value="${this.value}" id="duesValue">`)
+            // console.log(this.value, container)
+        })
+    }
+    function listenBackOption() {
+        $('#backA').click(function (e) {
+            let container = $('#amount-container')
+            container.empty()
+            container.append(`<input type="button" class="btn btn-info amount mr-2" name="amount"  value="5000">`)
+            container.append(`<input type="button" class="btn btn-info amount mr-2" name="amount"  value="3000">`)
+            container.append(`<input type="button" class="btn btn-info amount" name="amount" value="1000">`)
+            $('#amountValue').remove()
+            listenCreditOptions()
+        })
+        $('#backD').click(function (e) {
+            let container = $('#dues-container')
+            container.empty()
+            container.append(`<input type="button" class="btn btn-info dues mr-2" name="dues" value="5">`)
+            container.append(`<input type="button" class="btn btn-info dues mr-2" name="dues"  value="10">`)
+            container.append(`<input type="button" class="btn btn-info dues" name="dues"  value="20">`)
+            $('#duesValue').remove()
+            listenCreditOptions()
+        })
+    }
     $('#btn-cd').click(function (e) {
         const amountContainer = $('#amount-container')
         const duesContainer = $('#dues-container')
-        if(amountContainer.children().length > 1){
+        if (amountContainer.children().length > 2) {
             alert('Debes seleccionar el monto del prestamo')
             e.preventDefault()
             return
-        }if(duesContainer.children().length > 1){
+        } if (duesContainer.children().length > 2) {
             alert('Debes seleccionar la cantidad de cuotas')
             e.preventDefault()
             return
