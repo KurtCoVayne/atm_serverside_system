4b8a6352c15d3f867620f37d1a220dde728f0e59
KurtCoVayne <36391989+KurtCoVayne@users.noreply.github.com>
2019-10-05 22:47:38 -0400

    fixed db missconection

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://test0:dVxdMyx1HmhxM1sH@cluster0-shard-00-00-ojqdw.mongodb.net:27017,cluster0-shard-00-01-ojqdw.mongodb.net:27017,cluster0-shard-00-02-ojqdw.mongodb.net:27017/ATM_db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {
+mongoose.connect("mongodb+srv://nodeApplications:zT8GkAlf9kqkuPkc@cluster0-ojqdw.mongodb.net/test?retryWrites=true&w=majority", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
